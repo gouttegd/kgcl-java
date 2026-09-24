60649678421b748b0b1b8a988522712153c6fdf3
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2026-09-24 21:51:35 +0100

    Update the main class template.
    Update the class template based on the latest version of the Java
    generator in LinkML-Py.
    
    Also simply the custom generator script. We have all the features we
    need directly in the upstream generator, all we need is a small wrapper
    just to call it with the appropriate parameters.

--- core/src/main/linkml/scripts/javagen.py
+++ core/src/main/linkml/scripts/javagen.py
@@ -2,48 +2,15 @@
 
 import click
 from pathlib import Path
-from linkml.generators.javagen import JavaGenerator
+from linkml.generators.javagen import cli
 
 
-def cleanup_dir(directory: Path) -> None:
-    if directory.exists():
-        for file in directory.iterdir():
-            file.unlink()
-        directory.rmdir()
-
-
-@click.option("--output-directory",
-              type=click.Path(dir_okay=True, file_okay=False),
-              default=Path("core/src/main/java"))
-@click.option("--linkml-directory",
-              type=click.Path(exists=True, dir_okay=True, file_okay=False),
-              default=Path("core/src/main/linkml/schemas"))
-@click.command()
-def cli(output_directory, linkml_directory):
-
-    cleaned_up_dirs = {}
-
-    for schema in linkml_directory.glob("**/*.yaml"):
-        package_dir = schema.relative_to(linkml_directory).parent
-        output_dir = output_directory / package_dir
-        if output_dir not in cleaned_up_dirs:
-            cleanup_dir(output_dir)
-            cleaned_up_dirs[output_dir] = 1
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-        package_name = package_dir.as_posix().replace("/", ".")
-        gen = JavaGenerator(schema,
-                            true_enums=True,
-                            use_aliases=False,
-                            package=package_name,
-                            template_dir=Path("core/src/main/linkml/templates"))
-        visitors = []
-        if schema.name == "kgcl.yaml":
-            visitors = ["Change"]
-        gen.serialize(output_dir,
-                      visitors=visitors,
-                      template_variant="org.incenp.linkml")
-
-
-if __name__ == "__main__":
-    cli()
+ctx = click.Context(cli)
+ctx.invoke(cli,
+           yamlfile=Path("core/src/main/linkml/schemas"),
+           output_directory=Path("core/src/main/java"),
+           true_enums=True,
+           use_aliases=False,
+           template_variant="org.incenp.linkml",
+           template_dir=Path("core/src/main/linkml/templates"),
+           visitor=["Change"])
